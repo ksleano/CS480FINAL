c431ddd5664b2f1bd2e89e7c09272ee60e180e27
XBlade190 <XBlade190@gmail.com>
2017-03-15 21:51:59 -0700

    GUI with basic algorithm for DDOS

--- Log.java
+++ Log.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cs480.pkgclass.db;
+package cs480classprojectswingv2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -19,10 +19,11 @@ import java.util.logging.Logger;
  *
  * Log to hold entries
  */
-public class Log implements Comparable <Log>{
+public class Log {
     
     private String month;
     private String date;
+    private String subStr;
     private String src;
     private String dst;
     private String id;
@@ -34,22 +35,24 @@ public class Log implements Comparable <Log>{
     
     
     // counter 
-    private int ddosCounter;
+    private int ddosCounter = 0;
     private static int count = 0;
     private static int toStringIndexCounter = 0;
             
     public Log(String month, String date, String time, String src,
-               String dst, String id, String spt, String dpt, int count){
+               String dst, String id, String spt, String dpt){
         this.month = month;
         this.date = date;
+        //convert time to double
+//        subStr = time;
+//        subStr = subStr.substring(3);
+//        this.time = Double.parseDouble(subStr.replace(":","."));
         this.time = time;
         this.src = src;
         this.dst = dst;
         this.id = id;
         this.spt = spt;
         this.dpt = dpt;
-        this.ddosCounter = count;
-        
         
         //complete month date time format
         this.mdt = month + " " + date + " " + time;
@@ -58,20 +61,20 @@ public class Log implements Comparable <Log>{
     
     public String print() {
         return (month + " " + date + " " + time + " " + src + " " + dst 
-                + " " + id + " " + spt + " " + dpt );
+                + " " + id + " " + spt + " " + dpt + " ddosCount=" + ddosCounter);
     }
     
     // getters
     public String getIpSRC(){
         return this.src;
     }
+    
+    public String getDDOS() {
+        return Integer.toString(this.ddosCounter);
+    }
    
     public String getTime(){
         return this.mdt;
-    }
-    
-    public int getDDOScounter(){
-        return this.ddosCounter;
     }
     
     public void updateDDOScounter(){
@@ -91,20 +94,6 @@ public class Log implements Comparable <Log>{
         //System.out.println(date1 +  " " + date2 +  " "  + difference);
         
         return difference;
-    }
-
-    @Override
-    public int compareTo(Log otherLog) {
-        if (otherLog == null){
-            throw new NullPointerException("Comparing " + this + "to null");
-        } else if (!this.getClass().equals(otherLog.getClass())){
-            throw new ClassCastException("Possible ClassLoader issue");
-        } else if (this.ddosCounter > otherLog.getDDOScounter()){
-            return -1;
-        } else if (this.ddosCounter < otherLog.getDDOScounter()){
-            return 1;
-        }
-        return 0;
     }
    
     
